14ac64f94091944b633e6b70b17fa5d3e2b8a8ab
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-04-05 14:01:36 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ $link = mysqli_connect($host, $user, $pass, $db_name); // Соединяемся
 
 if(isset($_POST['submit'])){
   if(!empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['email']) && !empty($_POST['fileImg']) 
-  && !empty($_POST['Radios']) && !empty($_POST['CheckPrograming']) && !empty($_POST['CheckBusiness']) 
-  && !empty($_POST['CheckCook']) && !empty($_POST['CheckSelect'])&& !empty($_POST['message'])){
+  && !empty($_POST['Radios']) && (!empty($_POST['CheckPrograming']) || !empty($_POST['CheckBusiness']) 
+  || !empty($_POST['CheckCook'])) && !empty($_POST['CheckSelect']) && !empty($_POST['message'])){
 
     $name = $_POST['name'];
     $surname = $_POST['surname'];
@@ -29,9 +29,22 @@ if(isset($_POST['submit'])){
     $fileImg = $_POST['fileImg'];
     $Radios = $_POST['Radios'];
 
-    $CheckPrograming = $_POST['CheckPrograming'];
-    $CheckBusiness = $_POST['CheckBusiness'];
-    $CheckCook = $_POST['CheckCook'];
+    if (!empty($_POST['CheckPrograming'])){
+      $CheckPrograming = $_POST['CheckPrograming'];
+      $CheckBusiness = 'Нет';
+      $CheckCook = 'Нет';
+    }
+    else if (!empty($_POST['CheckBusiness'])){
+      $CheckPrograming = $_POST['Нет'];
+      $CheckBusiness = $_POST['CheckBusiness'];
+      $CheckCook = $_POST['Нет'];
+    }
+    else if (!empty($_POST['CheckCook'])){
+      $CheckPrograming = $_POST['Нет'];
+      $CheckBusiness = $_POST['Нет'];
+      $CheckCook = $_POST['CheckCook'];
+    }
+
     $CheckSelect = $_POST['CheckSelect'];
     $message = $_POST['message'];
 
@@ -46,7 +59,13 @@ if(isset($_POST['submit'])){
     else{
       echo "Form not submitted";
     }
-
+    // <?php 
+        // $sql = mysqli_query($link, 'SELECT `id`, `name`, `surname`, `email`, `fileImg`, `Radios`, `CheckPrograming`, `CheckBusiness`, `CheckCook`, `CheckSelect`, `message` FROM `users`');
+        
+        // while ($result = mysqli_fetch_array($sql)) {
+    //       echo "{$result['id']} {$result['name']} {$result['surname']} {$result['email']} {$result['fileImg']} {$result['Radios']} {$result['CheckPrograming']} {$result['CheckBusiness']} {$result['CheckCook']} {$result['CheckSelect']} {$result['message']}<br>";  
+    //     }
+    // 
   }
   else{
     echo "all fields required";
@@ -56,6 +75,7 @@ if(isset($_POST['submit'])){
 <table class="table">
   <thead>
     <tr>
+      
       <th scope="col">ID</th>
       <th scope="col">Имя</th>
       <th scope="col">Фамилия</th>
@@ -65,20 +85,33 @@ if(isset($_POST['submit'])){
       <th scope="col">Bool 1</th>
       <th scope="col">Bool 2</th>
       <th scope="col">Bool 3</th>
-      <th scope="col">Курс</th>
+      <th scope="col">Kypc</th>
       <th scope="col">Комментарий</th>
     </tr>
   </thead>
   <tbody>
+    <?php 
+      $sql = mysqli_query($link, 'SELECT `id`, `name`, `surname`, `email`, `fileImg`, `Radios`, `CheckPrograming`, `CheckBusiness`, `CheckCook`, `CheckSelect`, `message` FROM `users`');
+      $result = mysqli_fetch_array($sql);
+    ?>
+    <?php foreach ($sql as $result): ?>
+    <tr>
+        <th scope="row"><?php echo $result['id'] ?></th>
+        <td><?php echo $result['name'] ?></td>
+        <td><?php echo $result['surname'] ?></td>
+        <td><?php echo $result['email'] ?></td>
+        <td><?php echo $result['fileImg'] ?></td>
+        <td><?php echo $result['Radios'] ?></td>
+        <td><?php echo $result['CheckPrograming'] ?></td>
+        <td><?php echo $result['CheckBusiness'] ?></td>
+        <td><?php echo $result['CheckCook'] ?></td>
+        <td><?php echo $result['CheckSelect'] ?></td>
+        <td><?php echo $result['message'] ?></td>
+    </tr>
+    <?php endforeach; ?>  
   </tbody>
 </table>
-<?php 
-      $sql = mysqli_query($link, 'SELECT `id`, `name`, `surname`, `email`, `fileImg`, `Radios`, `CheckPrograming`, `CheckBusiness`, `CheckCook`, `CheckSelect`, `message` FROM `users`');
-      
-      while ($result = mysqli_fetch_array($sql)) {
-        echo "{$result['id']} {$result['name']} {$result['surname']} {$result['email']} {$result['fileImg']} {$result['Radios']} {$result['CheckPrograming']} {$result['CheckBusiness']} {$result['CheckCook']} {$result['CheckSelect']} {$result['message']}<br>";  
-      }
-?>
+
 <form action= "index.php" method= "GET"> 
   <input type="submit" value="Отправить результат на почту" class="btn btn-primary "/>
 </form>
@@ -93,3 +126,5 @@ if(isset($_POST['submit'])){
 	</section>
 </body>
 </html>
+
+
